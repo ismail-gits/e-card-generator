8d8fe387b55a1ce08b35c962f3aadd6673cf5ebf
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-09-20 03:23:29 +0530

    removed retreive card as it was not needed and saved once api request

--- backend/index.js
+++ backend/index.js
@@ -25,29 +25,13 @@ app.post('/generateCard', async (req, res) => {
     const { name, description, interests, linkedinUrl, xUrl } = req.body
 
     // Pusing card to mongodb
-    await Cards.create({
+    const card = await Cards.create({
         name,
         description,
         interests,
         linkedinUrl,
         xUrl
     })
-
-    res.json({message: "E-Card Generated!"})
-})
-
-// Retrieving card from mongodb
-app.get('/retrieveCard', async (req, res) => {
-    // input validation using zod
-    const input = req.body
-    const inputValidation = retrieveCardSchema.safeParse(input)
-
-    if (!inputValidation.success) {
-        console.log(inputValidation.error.errors)
-        return res.status(400).json({message: "Invalid id"})
-    }
-
-    const card = await Cards.findById(req.body.id)
 
     res.json({card})
 })
