c2d0503cfedcfdcbacfd97e1194a5691e4a34b72
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-09-20 00:25:42 +0530

    Added routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,57 @@
 const express = require('express')
 const { Cards } = require('./db')
-const { generateCard } = require('./validation')
+const { generateCardSchema, retrieveCardSchema } = require('./validation')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
+
+// Generating card and storing it on mongodb
+app.post('/generateCard', async (req, res) => {
+    // input validation using zod
+    const input = req.body
+    const inputValidation = generateCardSchema.safeParse(input)
+    
+
+    if (!inputValidation.success) {
+        console.log(inputValidation.error.errors)
+        return res.status(400).json({message: "Invalid inputs"})
+    }
+
+    const { name, description, interests, linkedinUrl, xUrl } = req.body
+
+    // Pusing card to mongodb
+    await Cards.create({
+        name,
+        description,
+        interests,
+        linkedinUrl,
+        xUrl
+    })
+
+    res.json({message: "E-Card Generated!"})
+})
+
+// Retrieving card from mongodb
+app.get('/retrieveCard', async (req, res) => {
+    // input validation using zod
+    const input = req.body
+    const inputValidation = retrieveCardSchema.safeParse(input)
+
+    if (!inputValidation.success) {
+        console.log(inputValidation.error.errors)
+        return res.status(400).json({message: "Invalid id"})
+    }
+
+    const card = await Cards.findById(req.body.id)
+
+    res.json({card})
+})
+
+app.listen(3000, () => {
+    console.log("App is listening on port 3000!")
+})
--- backend/validation.js
+++ backend/validation.js
@@ -1,13 +1,18 @@
 const zod = require('zod')
 
-const generateCard = zod.object({
+const generateCardSchema = zod.object({
     name: zod.string().min(1),
-    desription: zod.string().min(1),
-    interests: zod.array(),
+    description: zod.string().min(1),
+    interests: zod.array(zod.string()),
     linkedinUrl: zod.string().url(),
     xUrl: zod.string().url()
 })
 
+const retrieveCardSchema = zod.object({
+    id: zod.string().min(1)
+})
+
 module.exports = {
-    generateCard
+    generateCardSchema,
+    retrieveCardSchema
 }
